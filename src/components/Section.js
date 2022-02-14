4aebbb7ec408fb5f71273af6bf1e057be18dff43
engida32 <engida32lishan@gmail.com>
2022-02-15 02:19:36 +0300

    ADD:NavBar with styled-component and react-reveal

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal'
+import Pulse from 'react-reveal/Pulse'
+
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     return (
+        <Pulse >
         <Wrap bgImage={backgroundImg}>
+            <Fade bottom>
             <ItemText>
                 <h1> {title}</h1>
                 <p>{description}</p>
             </ItemText>
+            </Fade>
             <Buttons>
+                <Fade bottom>
                 <ButtonGroup>
                     <LeftButton>
                         {leftBtnText}
@@ -18,9 +25,11 @@ function Section({ title, description, leftBtnText, rightBtnText, backgroundImg 
                     </RightButton>}
 
                 </ButtonGroup>
+                </Fade>
                 <DownArrow src="/images/down-arrow.svg" />
             </Buttons>
         </Wrap>
+        </Pulse>
     )
 }
 
